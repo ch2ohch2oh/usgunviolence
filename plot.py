d60885b83d8c8809a3c3ff87a30df3126893d642
ch2ohch2oh <ch2ohch2oh@users.noreply.github.com>
2022-02-15 23:28:40 +0000

    Update by data-bot

--- plot.py
+++ plot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[16]:
 
 
 #!/usr/bin/env python3
@@ -18,7 +18,7 @@ df['date'] = pd.to_datetime(df['date'])
 df.head()
 
 
-# In[ ]:
+# In[17]:
 
 
 county_count = df[['state', 'county', 'death', 'injured']].groupby(by=['state', 'county']).sum()
@@ -27,7 +27,7 @@ county_count.sort_values(by='total', ascending=False, inplace=True)
 county_count.head()
 
 
-# In[ ]:
+# In[18]:
 
 
 def plot_victim_groupby(df, days=3, limit=20, groupby=['state', 'county']):
@@ -41,27 +41,28 @@ def plot_victim_groupby(df, days=3, limit=20, groupby=['state', 'county']):
         labels = count[:limit].index
     # print(labels)
     
-    fig = plt.figure(figsize=(8, 8))
+    fig = plt.figure(figsize=(12, 8))
     plt.barh(labels, count[:limit]['death'], label='death', color='black')
     plt.barh(labels, count[:limit]['injured'], left=count[:limit]['death'], label='injured', color='gray')
     plt.legend()
     plt.xlabel('Count')
     plt.ylabel('Location')
     plt.title(f'US gun violence victim count within the past {days} days')
+    plt.tight_layout()
     return fig
 
 fig = plot_victim_groupby(df)
 fig.savefig('docs/imgs/victim_city_3days.png')
 
 
-# In[ ]:
+# In[19]:
 
 
 fig = plot_victim_groupby(df, groupby=['state'])
 fig.savefig('docs/imgs/victim_state_3days.png')
 
 
-# In[ ]:
+# In[20]:
 
 
 date_count = df[['date', 'injured', 'death']].groupby('date').sum()
@@ -69,14 +70,14 @@ date_count['total'] = date_count['injured'] + date_count['death']
 date_count
 
 
-# In[ ]:
+# In[21]:
 
 
 def plot_victim_time(df, days=90):
     df = df[df.date > datetime.datetime.now() - pd.to_timedelta(f'{days}day')]
     date_count = df[['date', 'injured', 'death']].groupby('date').sum()
     date_count['total'] = date_count['injured'] + date_count['death']
-    fig = plt.figure(figsize=(8, 6))
+    fig = plt.figure(figsize=(12, 6))
     plt.bar(date_count.index, date_count['death'], label='death', color='black')
     plt.bar(date_count.index, date_count['injured'], bottom=date_count['death'], color='gray', label='injured')
     xlabels = [date.strftime('%Y-%m-%d') for date in date_count.index]
@@ -85,8 +86,15 @@ def plot_victim_time(df, days=90):
     plt.ylabel('Count')
     plt.title(f"Gun violence victim count within {days} days")
     plt.legend()
+    plt.tight_layout()
     return fig
 
 fig = plot_victim_time(df, days=90)
 fig.savefig('docs/imgs/victim_count_90days.png')
+
+
+# In[ ]:
+
+
+
 
